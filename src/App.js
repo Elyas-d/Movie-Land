51b1d31b9fbd851527cbc6e80d60b1a6c0f45f03
Elyas Damenu <elyas@venom>
2024-07-02 06:55:22 -0400

    deleted the boiler plate and started the real project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import './App.css'
+import SearchIcon from './search.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=60176cf';
+
+const App = () => {
+    const movie1 = {
+        "Title": "Spider-Man Title Reveal",
+        "Year": "2021",
+        "imdbID": "tt14122734",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNjRjMmQ2NDQtNmI5NC00N2EwLTkwYWQtOTM2OGZjMmI5YmRjXkEyXkFqcGdeQXVyMTI0NTA1MDI3._V1_SX300.jpg"
+      }
+    
+    const searchMovies = async(title) => {
+        const response = await fetch (`${API_URL}&s={title}`)
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(()=>{   
+        searchMovies('spiderman');
+    })
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="Search for movies" value="superman" onChange={()=>{}}/>
+                <img src={SearchIcon} alt="search" onChange={()=>{}}/>
+            </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'http://via.placeholder.com/400'} alt="Spider-Man Title Reveal"/>
+                    </div>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
