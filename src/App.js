543632bec092275d6192b3f3b4f0b0c25cc3e943
Elyas Damenu <elyas@venom>
2024-07-03 07:33:59 -0400

    cleaning code adding some features

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'http://www.omdbapi.com?apikey=60176cf';
 
-const movie1 = {
-  Title: "Spider-Man Title Reveal",
-  Year: "2021",
-  imdbID: "tt14122734",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNjRjMmQ2NDQtNmI5NC00N2EwLTkwYWQtOTM2OGZjMmI5YmRjXkEyXkFqcGdeQXVyMTI0NTA1MDI3._V1_SX300.jpg",
-};
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter"){
+      searchMovies(searchTerm);
+    }
+  };
+
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s={title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
   };
@@ -36,11 +33,13 @@ const App = () => {
           placeholder="Search for movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onkeydown={handleKeyDown}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchMovies(searchTerm)}
+          onClick={() => searchMovies(searchTerm)
+          }
         />
       </div>
       {movies?.length !== 0 ? (
@@ -54,9 +53,6 @@ const App = () => {
           <h2>No movies found</h2>
         </div>
       )}
-      {/* <div className="container">
-                <MovieCard movie1={movie1}/>
-            </div> */}
     </div>
   );
 };
